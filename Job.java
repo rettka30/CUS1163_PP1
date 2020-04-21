27c90904ecfad77fb6270513cf1045b4454956de
Andrew Rettko <andrew.rettko17@stjohns.edu>
2020-04-21 16:59:15 -0500

    Second Edit

---
+++ Job.java
@@ -0,0 +1,17 @@
+public class Job {
+
+   public String n;
+   public int t;
+   public int burst;
+
+   public Job(String name, int time, int CPUBurst) {
+      n = name;
+      t = time;
+      burst = CPUBurst;
+   }
+   
+   public String toString() {
+      return "Job #" + n + " => Time in : " + t + "; CPU Burst: " + burst;
+   }
+   
+}
---
+++ Simulation.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Simulation {
+
+   public static void main(String[] args){
+      File fd = new File("JobsToRun.txt");
+      
+   }
+
+}
